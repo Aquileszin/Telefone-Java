7249d37eac30299544d6fddb9042b3532f7ff0fa
Ekyto Aquiles <ekytoncavalcante@gmail.com>
2026-07-22 17:24:20 -0300

    Aqui criamos 2 Classes novas, uma Telefone que vai ser o centro da aplicação, e para dividir a tarefa de criar os contatos, criamos também a classe Contatos passando como atributo um ArrayList onde ficará nossos contatos salvos.

---
+++ src/Telefone/Contatos.java
@@ -0,0 +1,15 @@
+package Telefone;
+
+import java.util.ArrayList;
+
+public class Contatos {
+    private ArrayList<Contato> contatos;
+
+    public Contatos() {
+        contatos = new ArrayList<Contato>();
+    }
+
+    public ArrayList<Contato> getContatos() {
+        return contatos;
+    }
+}
---
+++ src/Telefone/Telefone.java
@@ -0,0 +1,9 @@
+package Telefone;
+
+public class Telefone {
+    private String numeroChip;
+
+    public Telefone(String numeroChip) {
+        this.numeroChip = numeroChip;
+    }
+}
