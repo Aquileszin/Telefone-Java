4b98ec190fdd5dde9ee30d29c54f975fe54c80c4
Ekyto Aquiles <ekytoncavalcante@gmail.com>
2026-07-22 17:00:38 -0300

    Aqui criamos a classe Contato com seus atributos, Construtores e Métodos Getters & Setters

---
+++ src/Telefone/Contato.java
@@ -0,0 +1,27 @@
+package Telefone;
+
+public class Contato {
+    private String nome;
+    private String numero;
+
+    public Contato(String nome, String numero) {
+        setNome(nome);
+        setNumero(numero);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
